2a3c66aa964bb7e96178d7c872a16376ff36b168
Sebastian Carazo <sebastian@lebas.dev>
2025-01-27 08:50:07 -0600

    use supabase client to insert to database

--- main.ts
+++ main.ts
@@ -1,7 +1,32 @@
 import "@std/dotenv/load";
 
+import { createClient } from "@supabase/supabase-js";
 import { useWebSocket } from "@effection-contrib/websocket";
 import { each, main } from "@effection/effection";
+import { generate } from "@babia/uuid-v7";
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(
+  Deno.env.get("SUPABASE_URL")!,
+  Deno.env.get("SUPABASE_ANON_KEY")!,
+);
+
+async function insertToSupabase() {
+  try {
+    const { error } = await supabase
+      .from("casio")
+      .insert({
+        id: generate(),
+        created_at: new Date().toISOString(),
+        title: "dummy test",
+      });
+
+    if (error) throw error;
+    console.log("Data inserted successfully");
+  } catch (error) {
+    console.error("Error inserting data:", error);
+  }
+}
 
 await main(function* () {
   const socket = yield* useWebSocket(Deno.env.get("WEBSOCKET_CLUSTER_URL")!);
@@ -10,6 +35,8 @@ await main(function* () {
     action: "auth",
     params: Deno.env.get("POLYGON_API_KEY"),
   }));
+
+  insertToSupabase();
 
   for (const message of yield* each(socket)) {
     const wsMessage = message as WebSocketMessageEvent;
